9bf78b79f52cae3f25bdc89c2a6daf52e49a0d7c
Satoshi Ogasawara <ogasawara@itpl.co.jp>
2016-11-05 17:36:00 +0900

    remove emacs settings.

--- call-toplevel.el
+++
@@ -1,4 +0,0 @@
-(require 'caml)
-(setq ocaml-project-root (file-name-directory load-file-name))
-(setq inferior-caml-program (concat "ocaml -init " ocaml-project-root "ocamlinit"))
-(caml-show-subshell)
